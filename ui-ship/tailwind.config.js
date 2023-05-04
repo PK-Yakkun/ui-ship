ce7bd51be859e6668cb350b047205766ca13de92
PK-Yakkun <mokkun515@gmail.com>
2023-05-05 00:41:22 +0900

    feat: themeにcolors定義

--- ui-ship/tailwind.config.js
+++ ui-ship/tailwind.config.js
@@ -2,6 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
   theme: {
+    colors: {
+      primary: "#bcc7d1",
+      secondary: "#142636",
+      typography: "#394a5a",
+    },
     extend: {},
   },
   plugins: [],
