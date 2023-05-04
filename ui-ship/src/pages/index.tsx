9ab3177bfaf4d4983bfefdf428a0f7f932f97bba
PK-Yakkun <mokkun515@gmail.com>
2023-05-04 17:12:37 +0900

    feat: ディレクトリ整理

---
+++ ui-ship/src/pages/index.tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from "next";
+
+const Home: NextPage = () => {
+  return (
+    <>
+      <menu>side menu</menu>
+      <section>main contents</section>
+    </>
+  );
+};
+
+export default Home;
