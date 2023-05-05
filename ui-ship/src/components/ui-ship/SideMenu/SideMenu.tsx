32e09fa46312904d5908cd2d5c0253d18ffef455
PK-Yakkun <mokkun515@gmail.com>
2023-05-06 00:29:36 +0900

    feat: SideMenu更新

--- ui-ship/src/components/ui-ship/SideMenu/SideMenu.tsx
+++ ui-ship/src/components/ui-ship/SideMenu/SideMenu.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import { useState } from "react";
 
 interface sideMenuItem {
   component: string;
@@ -6,33 +7,32 @@ interface sideMenuItem {
 
 const sideMenuItems: sideMenuItem[] = [
   {
-    component: "sample",
+    component: "Button",
   },
   {
-    component: "button",
+    component: "ScrollLine",
   },
   {
-    component: "menu",
-  },
-  {
-    component: "sample",
-  },
-  {
-    component: "sample",
-  },
-  {
-    component: "sample",
+    component: "SvgIcon",
   },
 ];
 
 function SideMenu() {
+  const [selectedName, setSelectedName] = useState<string>("");
+
   return (
     <menu className="w-72 min-h-screen bg-primary p-3">
       <h1 className="p-3 mb-3">UI Ship</h1>
       <ol>
         {sideMenuItems.map((item, index) => (
-          <li key={index} className="rounded-md overflow-hidden">
-            <Link href={item.component} passHref>
+          <li
+            key={index}
+            className={`rounded-md overflow-hidden transition duration-300 ${
+              item.component === selectedName ? "bg-tertiary" : ""
+            } hover:opacity-70`}
+            onClick={() => setSelectedName(item.component)}
+          >
+            <Link href={`/components/${item.component}`} passHref>
               <span className="block py-1 px-3">{item.component}</span>
             </Link>
           </li>
