11f64791af4add4521e7c1f2738351f487713915
PK-Yakkun <mokkun515@gmail.com>
2023-05-06 00:30:02 +0900

    add: ScrollLine追加

---
+++ ui-ship/src/components/ScrollLine/index.tsx
@@ -0,0 +1,26 @@
+function ScrollLine() {
+  return (
+    <div className="w-[2px] h-28 overflow-hidden before:content-'' before:block before:w-[2px] before:h-28 before:bg-primary before:animate-[scroll_1.4s_infinite]" />
+  );
+}
+
+export const code = `
+function ScrollLine() {
+  return (
+    <div className="w-[2px] h-28 overflow-hidden before:content-'' before:block before:w-[2px] before:h-28 before:bg-primary before:animate-[scroll_1.4s_infinite]" />
+  );
+}
+
+export default ScrollLine;
+
+/* tailwind.config.js */
+
+keyframes: {
+  scroll: {
+    "0%": { transform: "translateY(-100%)" },
+    "100%": { transform: "translateY(100%)" },
+  },
+},
+`;
+
+export default ScrollLine;
