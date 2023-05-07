78ee6c7d96340d6ea048c4313d370e7bf9a33b81
PK-Yakkun <mokkun515@gmail.com>
2023-05-07 12:02:08 +0900

    add: Buttonコンポーネント作成

---
+++ ui-ship/src/components/Button/index.tsx
@@ -0,0 +1,21 @@
+function Button() {
+  return (
+    <button className="bg-primary py-3 px-6 rounded-md hover:opacity-75">
+      Button
+    </button>
+  );
+}
+
+export const code = `
+function Button() {
+  return (
+    <button className="bg-primary py-3 px-6 rounded-md hover:opacity-75">
+      Button
+    </button>
+  );
+}
+
+export default Button;
+`;
+
+export default Button;
