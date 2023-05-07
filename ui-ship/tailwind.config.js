720ca869ec2629d70394896f4c0f6ab2d953492e
PK-Yakkun <mokkun515@gmail.com>
2023-05-07 12:01:04 +0900

    feat: themeにブレイクポイント追加

--- ui-ship/tailwind.config.js
+++ ui-ship/tailwind.config.js
@@ -2,6 +2,9 @@
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
   theme: {
+    screens: {
+      md: "900px",
+    },
     colors: {
       primary: "#bcc7d1",
       secondary: "#142636",
